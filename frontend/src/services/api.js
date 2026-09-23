58174ca81ac9bc7e864aafecea22cc789d6dbe52
Bhavesh Kapil <bhaveshkapil09@gmail.com>
2026-09-23 15:08:04 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://metrika-legal-metrology.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
